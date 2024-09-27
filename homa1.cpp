f14215227e97f03bf093eb0ae5a79d8b75bbf8e9
AObernihin <arsenobernihin@gmail.com>
2024-09-28 00:35:24 +0300

    Добавьте файлы проекта.

---
+++ homa1.cpp
@@ -0,0 +1,55 @@
+﻿using namespace std;
+
+#include <iostream>
+
+int main()
+{
+	const int col = 3, row = 3;
+	int a, temp, multi;
+	int arr[row][col];
+	cout << "enter first:";
+	cin >> a;
+	arr[0][0] = a;
+	for (int x = 0; x < 3; x++)
+	{
+		for (int y = 0; y < 3; y++)
+		{
+			if (arr[x][y] == a)
+			{
+				cout << arr[x][y] << " ";
+				continue;
+			}
+			if (y == 0)
+			{
+				arr[x][y] = arr[x - 1][col] * 2;
+			}
+			arr[x][y] = arr[x][y - 1] * 2;
+			cout << arr[x][y] << " ";
+		}
+		cout << endl;
+	}
+	//=========================//
+	cout << "enter first:";
+	cin >> a;
+	arr[0][0] = a;
+	for (int x = 0; x < 3; x++)
+	{
+		for (int y = 0; y < 3; y++)
+		{
+			if (arr[x][y] == a)
+			{
+				cout << arr[x][y] << " ";
+				continue;
+			}
+			if (y == 0)
+			{
+				arr[x][y] = arr[x - 1][col] + 1;
+			}
+			arr[x][y] = arr[x][y - 1] + 1;
+			cout << arr[x][y] << " ";
+		}
+		cout << endl;
+	}
+	//==================//
+}
+
